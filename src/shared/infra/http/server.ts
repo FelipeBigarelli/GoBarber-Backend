f36d1c167711f19ba2a99a42b55149e9359e0c7d
FelipeBigarelli <felipebigarelli@hotmail.com>
2021-02-26 12:50:08 -0300

    .

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ const app = express();
 // Cors: evita que alguns sites que não sejam confiáveis da aplicação possam acessar o site
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
